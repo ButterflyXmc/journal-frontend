34eaaafd588fd2d394565825216d83278447ed7d
mahic <mahic@domain.com>
2023-03-07 17:31:59 -0700

    first commit

---
+++ src/views/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Home Page</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HomePage"
+    }
+</script>
+
+<style scoped>
+    h1{
+        text-align: center;
+    }
+</style>
